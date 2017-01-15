923b89b0742444bf4ed670c76d0079be74544852
eagraf <graf.ethan@gmail.com>
2017-01-15 16:47:25 -0500

    Implemented basic group schema.

--- models/schemas/group.js
+++ models/schemas/group.js
@@ -2,19 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 var groupSchema = new Schema({
-        firstName: {type: String, required: true, trim: true, sparse: true},
-        lastName: {type: String, required: true, trim: true, sparse: true},
-        email: {type: String, trim: true, required: true, unique: true, sparse: true},
-        phone: {type: String, sparse: true},
-        hash: {type: String, required: true},
-        bio: String,
-        isVerified: Boolean,
-        token: String,
-        resetToken: String,
-        contacts: [{
-            contactId: Schema.ObjectId,
+        name: {type: String, required: true, trim: true, sparse: true},
+        description: String,
+        background: String,
+        members: [{
+            memberId: Schema.ObjectId,
             name: String,
             _id: false
+        }],
+        conversations: [{
+            conversationId: Schema.ObjectId,
         }],
         groups: [{
             groupId: Schema.ObjectId,
