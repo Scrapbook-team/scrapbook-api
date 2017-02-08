dee0bedc435b36bf0845ac23e308eaad64147cfe
eagraf <graf.ethan@gmail.com>
2017-02-07 19:51:46 -0500

    Fixed content type for file uploads.

--- controllers/upload.js
+++ controllers/upload.js
@@ -16,7 +16,7 @@ exports.upload = multer({
         s3,
         bucket: 'scrapbook-testing',
         acl: 'public-read',
-        contentType: file.contentType,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
         metadata(req, file, cb) {
             cb(null, {fieldName: file.fieldname});
         },
